9e1b6775ff89b00d669516403a959d3f1367630b
jlundgren77 <jlundgren77@gmail.com>
2020-02-10 10:53:28 -0500

    fixed type bug

--- src/models/user.js
+++ src/models/user.js
@@ -60,7 +60,7 @@ UserSchema.virtual('tasks', {
 });
 UserSchema.methods.generateAuthToken = async function() {
   const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, proecss.env.JWT_SECRET);
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
--- src/routers/user.js
+++ src/routers/user.js
@@ -25,7 +25,9 @@ router.post('/users/login', async (req, res) => {
       req.body.email,
       req.body.password
     );
+
     const token = await user.generateAuthToken();
+
     res.send({ user, token });
   } catch (e) {
     res.status(400).send('unable to login');
