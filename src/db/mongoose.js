35a6edc28d6c783d702bff5f605de88a1a87e21d
jlundgren77 <jlundgren77@gmail.com>
2020-02-10 10:27:00 -0500

    add error check

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,11 +1,21 @@
 var mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://${process.env.MONGODB_URL}`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose
+  .connect(`mongodb://${process.env.MONGODB_URL}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log(
+      'MongoDB successfully connected. Database URL:',
+      process.env.MONGODB_URL
+    );
+  })
+  .catch(error => {
+    console.log('Error establishing MongoDB connection. Error:', error.message);
+  });
 
 // var task1 = new Task({
 //   description: 'Get job right now, this morning          '
